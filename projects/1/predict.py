737515b5a36dbb6b5e6c24750e2577e90e435b5c
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2019-10-28 14:54:34 +0300

    Update predict.py

--- projects/1/predict.py
+++ projects/1/predict.py
@@ -25,7 +25,7 @@ model = load("1.joblib")
 #read and infere
 read_opts=dict(
         sep=',', names=fields, index_col=False, header=None,
-        iterator=True, chunksize=100
+        iterator=True, chunksize=200
 )
 
 for df in pd.read_csv(sys.stdin, **read_opts):
