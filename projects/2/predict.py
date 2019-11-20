4b8a1634b01895f4a7c68b69934af4c1cfb0476d
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2019-11-20 18:47:49 +0300

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -37,20 +37,20 @@ for line in sys.stdin:
         id, if1, if2, if3, if4, if5,\
         if6, if7, if8, if9, if10, if11, if12,\
         if13, if14, cf2, cf3, cf4 = line.strip('\n').split('\s')
-        if if1 = '\\N': if1 = 0
-        if if2 = '\\N': if2 = 0
-        if if3 = '\\N': if3 = 0
-        if if4 = '\\N': if4 = 0
-        if if5 = '\\N': if5 = 0
-        if if6 = '\\N': if6 = 0
-        if if7 = '\\N': if7 = 0
-        if if8 = '\\N': if8 = 0
-        if if9 = '\\N': if9 = 0
-        if if10 = '\\N': if10 = 0
-        if if11 = '\\N': if11 = 0
-        if if12 = '\\N': if12 = 0
-        if if13 = '\\N': if13 = 0
-        if if14 = '\\N': if14 = 0
+        if if1 = '\\N': if1 = '0'
+        if if2 = '\\N': if2 = '0'
+        if if3 = '\\N': if3 = '0'
+        if if4 = '\\N': if4 = '0'
+        if if5 = '\\N': if5 = '0'
+        if if6 = '\\N': if6 = '0'
+        if if7 = '\\N': if7 = '0'
+        if if8 = '\\N': if8 = '0'
+        if if9 = '\\N': if9 = '0'
+        if if10 = '\\N': if10 = '0'
+        if if11 = '\\N': if11 = '0'
+        if if12 = '\\N': if12 = '0'
+        if if13 = '\\N': if13 = '0'
+        if if14 = '\\N': if14 = '0'
         if cf2 = '\\N': cf2 =''
         if cf3 = '\\N': cf3 =''
         if cf4 = '\\N': cf4 =''
