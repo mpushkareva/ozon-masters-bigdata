bbb85d70bbc9fa6012e32f2a7570c937e9bf779a
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2019-11-22 13:04:25 +0200

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -25,13 +25,13 @@ numeric_features = ["if"+str(i) for i in range(1,14)]
 categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
 
 fields_without_label = ["id"] + numeric_features + categorical_features
-
-read_opts=dict(
-        sep='\t', names=fields_without_label, index_col=False, header=None,
-        iterator=True, chunksize=1000
-)
 fields_selected = ["id"] + ["if"+str(i) for i in range(1,14)] \
                 + ["cf2", "cf3", "cf4"]
+read_opts=dict(
+        sep='\t', names=fields_selected, index_col=False, header=None,
+        iterator=True, chunksize=1000
+)
+
 for df in pd.read_csv(sys.stdin, **read_opts):
     df.iloc[:, 0:14] = df.iloc[:, 0:14].replace('\\N', '0')
     df.iloc[:, 0:14] = df.iloc[:, 0:14].astype(int)
