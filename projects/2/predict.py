1ad0a36cd75d6129c5ea7c89884ee26b3419928e
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2019-11-22 16:18:06 +0200

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -35,7 +35,7 @@ read_opts=dict(
 for df in pd.read_csv(sys.stdin, **read_opts):
     df_selected = df.loc[fields_selected]
     df_selected.iloc[:, 0:14] = df_selected.iloc[:, 0:14].replace('\\N', '0')
-    df_selected.iloc[:, 0:14] = df_selected.iloc[:, 0:14].astype(int)
+    df_selected.iloc[:, 0:14] = pd.to_numeric(df_selected.iloc[:, 0:14])
     pred = model.predict(df_selected)
     out = zip(df_selected.id, pred)
     print("\n".join(["{0}\t{1}".format(*i) for i in out]))
