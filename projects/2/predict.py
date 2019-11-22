00c0a20d449e9a358ff104de057a2f462764f190
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2019-11-22 12:28:13 +0200

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -33,9 +33,8 @@ read_opts=dict(
 fields_selected = ["id"] + ["if"+str(i) for i in range(1,14)] \
                 + ["cf2", "cf3", "cf4"]
 for df in pd.read_csv(sys.stdin, **read_opts):
-    df_selected = df.loc[:, fields_selected]
-    df_selected.iloc[:, 0:15].replace('\\N', '0')
-    df_selected.iloc[:, 0:15] = df_selected.iloc[:, 0:15].astype(int)
-    pred = model.predict(df_selected)
-    out = zip(df_selected.id, pred)
+    df.iloc[:, 0:15].replace('\\N', '0')
+    df.iloc[:, 0:15] = df.iloc[:, 0:15].astype(int)
+    pred = model.predict(df)
+    out = zip(df.id, pred)
     print("\n".join(["{0}\t{1}".format(*i) for i in out]))
