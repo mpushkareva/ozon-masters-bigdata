b4b53ec094420f7b7167a6e71aa90627c5b6e5c5
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2019-11-22 10:51:47 +0200

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -35,8 +35,8 @@ fields_selected = ["id"] + ["if"+str(i) for i in range(1,14)] \
 
 for line in sys.stdin:
         df = pd.DataFrame(line.strip('\n').split('\t'))
-        df.iloc[0:15].replace('\\N', '0')
-        df.iloc[15:18].replace('\\N', '')
+        df.iloc[:,0:15].replace('\\N', '0')
+        df.iloc[:,0:15] = df.iloc[:,0:15].astype(int)
         pred = model.predict(df)
         out = zip(df[0][0], pred)
-        print("{0}\t{1}".format(*i) for i in out))
+        print("\n".join(["{0}\t{1}".format(*i) for i in out]))
