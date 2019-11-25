8819329941f3640669d0a974e2129e0e608eb8c0
mpushkareva <55574266+mpushkareva@users.noreply.github.com>
2019-11-25 08:19:47 +0200

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -37,6 +37,6 @@ for df in pd.read_csv(sys.stdin, **read_opts):
     df_selected.iloc[:, 0:14] = df_selected.iloc[:, 0:14].replace('\\N', '0')
     df_selected.iloc[:, 0:14] = pd.to_numeric(df_selected.iloc[:, 0:14])
     df_selected.iloc[:, 14:17] = df_selected.iloc[:, 14:17].replace('\\N', '')
-    pred = model.predict_probe(df_selected)
+    pred = model.predict(df_selected)
     out = zip(df_selected.id, pred)
     print("\n".join(["{0}\t{1}".format(*i) for i in out]))
